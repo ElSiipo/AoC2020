e921fbcac2e03e4d88a09caed515084d0be92a25
Christian Bartelius <cbartelius@gmail.com>
2020-12-04 11:45:28 +0100

    Adding days to github

---
+++ AoC2020/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace AoC_2020_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day4.Run();
+        }
+    }
+}
